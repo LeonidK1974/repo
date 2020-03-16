9a8283263bbfccd098307108534a6fa0e5150e7b
Leon <leonid.i.kudryavtsev@gmail.com>
2020-03-16 23:46:30 +0300

    Implement task RectangleClass declare

---
+++ Coursera/C++Specialization/CourseraYellowBelt/RectangleClassDeclaration/RectangleClassDeclaration.cpp
@@ -0,0 +1,18 @@
+﻿// RectangleClassDeclaration.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Rectangle.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello Rect!\n";
+	Rectangle r(3, 4);
+	cout << r.GetHeight() << endl;
+	cout << r.GetWidth() << endl;
+	cout << r.GetArea() << endl;
+	cout << r.GetPerimeter() << endl;
+}
+
