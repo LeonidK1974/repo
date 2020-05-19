92f0afc0ad18058adbdb380030c950dc7eb570b3
Leon <leonid.i.kudryavtsev@gmail.com>
2020-05-19 18:45:49 +0300

    done task SimpleVector with no copy

--- Coursera/C++Specialization/CourseraRedBelt/SimpleVector2/simple_vector_2.h
+++ Coursera/C++Specialization/CourseraRedBelt/SimpleVector2/simple_vector_2.h
@@ -1,25 +1,32 @@
-#pragma once
-
 #include <algorithm>
+#include <cstdint>
+
 using namespace std;
+
+// Реализуйте SimpleVector в этом файле
+// и отправьте его на проверку
 
 template <typename T>
 class SimpleVector {
 public:
-	SimpleVector() = default;
-	explicit SimpleVector(size_t size);
-	~SimpleVector();
+  SimpleVector() = default;
+  explicit SimpleVector(size_t size);
+  ~SimpleVector();
 
-	T& operator[](size_t index);
+  T& operator[](size_t index);
 
-	T* begin();
-	T* end();
+  T* begin();
+  T* end();
 
-	size_t Size() const;
-	size_t Capacity() const;
-	void PushBack(const T& value);
+  size_t Size() const;
+  size_t Capacity() const;
+  void PushBack(T value);
+
+  // При необходимости перегрузите
+  // существующие публичные методы
 
 private:
+  // Добавьте сюда поля
 	T* data = nullptr;
 	size_t size = 0;
 	size_t capacity = 0;
@@ -54,16 +61,16 @@ size_t SimpleVector<T>::Capacity() const {
 }
 
 template <typename T>
-void SimpleVector<T>::PushBack(const T& value) {
+void SimpleVector<T>::PushBack(T value) {
 	if (size >= capacity) {
 		auto new_cap = capacity == 0 ? 1 : 2 * capacity;
 		auto new_data = new T[new_cap];
-		copy(begin(), end(), new_data);
+		move(begin(), end(), new_data);
 		delete[] data;
 		data = new_data;
 		capacity = new_cap;
 	}
-	data[size++] = value;
+	data[size++] = move(value);
 }
 
 template <typename T>
