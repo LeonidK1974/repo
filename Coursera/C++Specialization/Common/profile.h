bac847d303c108a0e9b6dc6ae94a20db7d030a93
Leon <leonid.i.kudryavtsev@gmail.com>
2020-06-09 20:10:34 +0300

    some changes

--- Coursera/C++Specialization/Common/profile.h
+++ Coursera/C++Specialization/Common/profile.h
@@ -1,35 +1,38 @@
 #pragma once
-#pragma once
 
 #include <chrono>
 #include <iostream>
+#include <optional>
 #include <string>
-
-using namespace std;
-using namespace std::chrono;
+#include <sstream>
 
 class LogDuration {
 public:
-	explicit LogDuration(const string& msg = "")
-		: message(msg + ": ")
-		, start(steady_clock::now())
-	{
-	}
+  explicit LogDuration(const std::string& msg = "")
+    : message(msg + ": ")
+    , start(std::chrono::steady_clock::now())
+  {
+  }
 
-	~LogDuration() {
-		auto finish = steady_clock::now();
-		auto dur = finish - start;
-		cerr << message
-			<< duration_cast<milliseconds>(dur).count()
-			<< " ms" << endl;
-	}
+  ~LogDuration() {
+    auto finish = std::chrono::steady_clock::now();
+    auto dur = finish - start;
+    std::ostringstream os;
+    os << message
+       << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
+       << " ms" << std::endl;
+    std::cerr << os.str();
+  }
 private:
-	string message;
-	steady_clock::time_point start;
+  std::string message;
+  std::chrono::steady_clock::time_point start;
 };
 
-#define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
-#define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
+#ifndef UNIQ_ID
+  #define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
+  #define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
+#endif
 
 #define LOG_DURATION(message) \
   LogDuration UNIQ_ID(__LINE__){message};
+
